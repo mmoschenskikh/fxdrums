75d6c6b946ddc28135733dfbdab2d714b0063d6c
Maxim Moschenskikh <m.moschenskikh@gmail.com>
2020-06-11 00:31:53 +0300

    Project set up to build .jar files

---
+++ src/main/java/ru/spbstu/fxdrums/Main.java
@@ -0,0 +1,10 @@
+package ru.spbstu.fxdrums;
+
+/**
+ * A class to build proper .jar files with Maven.
+ */
+public class Main {
+    public static void main(String[] args) {
+        DrumsApp.main(args);
+    }
+}
